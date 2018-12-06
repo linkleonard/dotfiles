557dcac51b088564e7963e8b0ab4a75d8fb54c7f
Leonard Law <leon.wlaw@gmail.com>
2018-12-05 22:53:05 -0500

    Add vscode install to windows install script

--- bin/install-dotfiles.ps1
+++ bin/install-dotfiles.ps1
@@ -1,5 +1,7 @@
 # Need to run this as an administrator... :/
 new-item -type symboliclink -path D:\Documents\.emacs -value D:\Documents\dotfiles\emacs\emacs
+New-Item -Path ~\AppData\Roaming\Code\User\settings.json -ItemType SymbolicLink -Value D:\Documents\dotfiles\vscode\settings.json
+New-Item -Path ~\AppData\Roaming\Code\User\keybindings.json -ItemType SymbolicLink -Value D:\Documents\dotfiles\vscode\keybindings.json
 
 # Command line tools
 choco install rg
